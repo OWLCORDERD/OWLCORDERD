c604df72ade5798d4aea9460ec93350a77f4c382
MIYEOK <lim5570@naver.com>
2024-03-12 18:39:25 +0900

    Main Page React-Helmet Meta Tag 제거 -> public/index.html 파일로 meta 태그 이동

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import 'asset/styles/main.scss';
 import Navbar from 'Components/Navigator/navbar';
 import ResponsiveMenu from 'Components/Navigator/MobileResponsive/ResponsiveMenu';
-import { Helmet } from 'react-helmet-async';
 import Project from 'Components/About/Project';
 import ScrollToTop from 'CustomHook/ScrollToTop';
 import Footer from '../Components/Footer/Footer';
@@ -15,19 +14,6 @@ function Main() {
   return (
     <>
       <ScrollToTop />
-      <Helmet>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="퍼블리싱부터 프론트엔드 개발까지 html, css, scss, javascript, typescript 등 다양한 언어와 다양한 애니메이션 라이브러리를 활용한 퍼블리싱을 통해 인터렉티브한 페이지 구현을 매일 더 발전하기 위해 노력하는 개발자 OWLCODERD, 임민혁입니다. 현재 개발 중인 프로젝트들을 확인하실 수 있습니다."
-        />
-        <meta
-          name="keywords"
-          content="Front-end, developer, 프론트엔드 개발자, 웹 개발자, Publisher, Publishing, 포트폴리오, 웹 사이트"
-        />
-
-        <title>임민혁 · OWLCODERD · Front-end Developer</title>
-      </Helmet>
       <header>
         <Navbar />
       </header>
