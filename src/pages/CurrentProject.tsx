3eb8600ca04d8401b48545490798c0587ce0eb88
MIYEOK <lim5570@naver.com>
2024-06-15 01:59:10 +0900

    06.15 Fixed: 프로젝트 상세 페이지에 scrollToTop 커스텀 훅 추가하여 상세 페이지에 접속 시 상단 위치 고정 & 모바일 반응형 UI 버그 수정

--- src/pages/CurrentProject.tsx
+++ src/pages/CurrentProject.tsx
@@ -10,6 +10,7 @@ import { Helmet } from 'react-helmet-async';
 import { ProjectType } from 'api/CommonService';
 import Footer from 'Components/Footer/Footer';
 import Loading from 'CustomHook/Loading';
+import ScrollToTop from 'CustomHook/ScrollToTop';
 import ProjectBoard from '../Components/CurrentProject/ProjectBoard/ProjectBoard';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -75,64 +76,67 @@ function CurrentProject() {
   };
 
   return (
-    <div className="container">
-      <Helmet>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content={`${projectDB?.title} 프로젝트는 ${projectDB?.subTitle}입니다. 해당 프로젝트의 소개 및 구현된 페이지들을 상세히 확인하실 수 있습니다.`}
-        />
-        <meta
-          name="keywords"
-          content="Front-end, developer, 프론트엔드 개발자, 웹 개발자, Publisher, Publishing, 포트폴리오, 웹 사이트"
-        />
+    <>
+      <ScrollToTop />
+      <div className="container">
+        <Helmet>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content={`${projectDB?.title} 프로젝트는 ${projectDB?.subTitle}입니다. 해당 프로젝트의 소개 및 구현된 페이지들을 상세히 확인하실 수 있습니다.`}
+          />
+          <meta
+            name="keywords"
+            content="Front-end, developer, 프론트엔드 개발자, 웹 개발자, Publisher, Publishing, 포트폴리오, 웹 사이트"
+          />
 
-        <title>{`${projectDB?.title} Project`}</title>
-      </Helmet>
-      <header>
-        <Navbar />
-      </header>
+          <title>{`${projectDB?.title} Project`}</title>
+        </Helmet>
+        <header>
+          <Navbar />
+        </header>
 
-      <ResponsiveMenu />
+        <ResponsiveMenu />
 
-      {!loading ? (
-        <main>
-          <div className="currentProject-container">
-            {openVideo === true ? (
-              <motion.div className="video-popup" variants={videoContainerOpen} initial="initial" animate="animate">
-                <button type="button" className="close-button" onClick={closeVideo}>
-                  close
-                </button>
+        {!loading ? (
+          <main>
+            <div className="currentProject-container">
+              {openVideo === true ? (
+                <motion.div className="video-popup" variants={videoContainerOpen} initial="initial" animate="animate">
+                  <button type="button" className="close-button" onClick={closeVideo}>
+                    close
+                  </button>
 
-                <motion.div
-                  className="project-video"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ delay: 1, duration: 1 }}
-                >
-                  <video controls src={projectDB.video} autoPlay muted loop />
+                  <motion.div
+                    className="project-video"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ delay: 1, duration: 1 }}
+                  >
+                    <video controls src={projectDB.video} autoPlay muted loop />
+                  </motion.div>
                 </motion.div>
-              </motion.div>
-            ) : null}
+              ) : null}
 
-            <div className="project-banner">
-              <ProjectInfo
-                currentData={projectDB}
-                setOpen={setOpenVideo}
-                open={openVideo}
-                responsiveMatches={responsiveMatches}
-              />
+              <div className="project-banner">
+                <ProjectInfo
+                  currentData={projectDB}
+                  setOpen={setOpenVideo}
+                  open={openVideo}
+                  responsiveMatches={responsiveMatches}
+                />
+              </div>
+
+              <ProjectBoard currentData={projectDB} />
             </div>
 
-            <ProjectBoard currentData={projectDB} />
-          </div>
-
-          <Footer />
-        </main>
-      ) : (
-        <Loading />
-      )}
-    </div>
+            <Footer />
+          </main>
+        ) : (
+          <Loading />
+        )}
+      </div>
+    </>
   );
 }
 
